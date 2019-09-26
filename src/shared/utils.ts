07dc07c74029c62be98457348733ab35da6e340f
Alexis Arancibia <alexis2396@hotmail.com>
2019-09-26 09:50:14 -0500

    Prueba

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,18 +1,22 @@
 import * as moment from 'moment-timezone';
 
-export const formatFechaCorta = ( fecha?: any ) => {
-  const fechaFormateada =  moment( fecha ? fecha : new Date() ).tz('America/Lima').format( 'YYYY-MM-DD');
+export const formatFechaCorta = (fecha?: any) => {
+  const fechaFormateada = moment(fecha ? fecha : new Date())
+    .tz('America/Lima')
+    .format('YYYY-MM-DD');
   return fechaFormateada;
 };
 
-export const formatFechaLarga = ( fecha?: any ) => {
-  const fechaFormateada =  moment( fecha ? fecha : new Date() ).tz('America/Lima').format( 'YYYY-MM-DD HH:mm:ss:SSS');
+export const formatFechaLarga = (fecha?: any) => {
+  const fechaFormateada = moment(fecha ? fecha : new Date())
+    .tz('America/Lima')
+    .format('YYYY-MM-DD HH:mm:ss:SSS');
   return fechaFormateada;
 };
 
-export const formatHora = ( fecha?: any ) => {
-  const fechaFormateada =  moment( fecha ? fecha : new Date() ).tz('America/Lima').format( 'HH:mm:SS');
+export const formatHora = (fecha?: any) => {
+  const fechaFormateada = moment(fecha ? fecha : new Date())
+    .tz('America/Lima')
+    .format('HH:mm:SS');
   return fechaFormateada;
 };
-
-export const otraFuncion = () => {}
